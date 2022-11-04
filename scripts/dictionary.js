e624b2b92d74c7602cff7d60d726d716b29f62c6
Bhashwar <bhaswarsengupta@gmail.com>
2022-11-04 11:38:01 +0530

    Got input from the webpage into the background script

--- scripts/dictionary.js
+++ scripts/dictionary.js
@@ -1,13 +1,42 @@
 chrome.contextMenus.create(
     {
         id: "foo",
-        title: "click",
+        title: "Search %s",
         contexts: ["selection"]
     }
 );
 
-chrome.contextMenus.onClicked.addListener(
-    function(clickData){
-        console.log(clickData.selectionText);
-    }
-);
+// chrome.contextMenus.onClicked.addListener(
+//     function(clickData){
+//         fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${clickData.selectionText}`)
+//         .then((response) => response.json())
+//         .then((result) => {
+//             console.log(result);
+//         });
+//     }
+// );
+
+chrome.contextMenus.onClicked.addListener((info, tab) => {
+    // Service worker cannot access the DOM, so alert() isn't available.
+    //  alert("Hello");
+    chrome.windows.create({
+      width: 350,
+      height: 250,
+      top: 140,
+      left: 1000,
+      type: "popup",
+      url: "alert.html"
+    });
+    console.log("Hello");
+    // chrome.runtime.sendMessage({"defn": "hola"});
+});
+
+
+
+
+
+
+
+
+
+
